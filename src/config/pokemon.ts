5703d2b9dc705097925995031644b17615106d3a
Nurimansyah Rifwan <nurimansyah.rifwan@gmail.com>
2019-07-22 15:27:55 +0700

    chore: update image url default value on configuration

--- src/config/pokemon.ts
+++ src/config/pokemon.ts
@@ -3,7 +3,7 @@ export default {
   apiUrl: process.env.REACT_APP_POKEMON_API_URL || 'https://pokeapi.co/api/v2',
 
   // Pokemon Image URL Endpoint
-  imageUrl: process.env.REACT_APP_POKEMON_IMAGE_URL || 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail',
+  imageUrl: process.env.REACT_APP_POKEMON_IMAGE_URL || 'https://assets.pokemon.com/assets/cms2/img/pokedex',
 
   // We assign manually because the api isn't completed yet
   totalPokemons: process.env.REACT_APP_POKEMON_TOTAL || 807,
